11eae6835e4b60cc095101f0b932f8401d8c2668
irenlian <irynalian@stockx.com>
2021-08-13 12:11:01 +0300

    Add initial docs

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # hotelbook
 Back-end created to provide API for teaching purposes in Star Academy
+
+Staging URL: https://hotel-booking-server-api.herokuapp.com/
+
+Swagger: https://app.swaggerhub.com/apis/web-academy/Hotelbook/1.0.0-oas3
--- app/app.ts
+++ app/app.ts
@@ -18,9 +18,10 @@ const options:cors.CorsOptions = {
   preflightContinue: false,
 };
 
-app.use(cors(options));
+app.use(cors());
 
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(errorHandling);
 initRoutes(app);
